03b80eb59897a23d6f0b40a212f7479ba2bb859d
Michael Baudis <m@baud.is>
2026-05-15 16:01:49 +0200

    Refactor content type parsing to use email.Message
    Replaced cgi with email.message.Message for content type handling.
    
    Based on https://github.com/jacksmith15/json-ref-dict/pull/30/changes/404448f6d7baa30a2a59abbadbc1df61fc980f62 to avoid `cgi` dependency

--- json_ref_dict/loader.py
+++ json_ref_dict/loader.py
@@ -1,4 +1,4 @@
-import cgi
+from email.message import Message
 import os
 import pathlib
 import posixpath
@@ -139,8 +139,9 @@ def _get_content_type(conn) -> str:
     content_type = mimetypes.guess_type(conn.url)[0] or ""
     if hasattr(conn, "getheaders"):
         content_type = dict(conn.getheaders()).get("Content-Type", content_type)
-    return cgi.parse_header(content_type)[0]
-
+    msg = Message()
+    msg['content-type'] = content_type
+    return msg.get_content_type()
 
 # Can possibly be hotswitched by patching.
 loader: Loader = Loader(default_get_document)
